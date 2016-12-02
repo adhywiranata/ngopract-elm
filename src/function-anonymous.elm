d90d43f6e8634c03628dfbfc3e2908a4681e2929
Adhy <adhywiranatap@gmail.com>
2016-12-02 22:45:21 +0700

    added anonymous function elm

--- src/function-anonymous.elm
+++ src/function-anonymous.elm
@@ -1,16 +1,10 @@
-module NamedFunction exposing (..)
+module AnonymousFunction exposing (..)
 
 import Html exposing (text)
 
--- function signature (this is optional)
--- [read: int as a parameter 1 and 2, and int as the return]
-sum : Int -> Int -> Int
--- named function
-sum x y =
-  x + y
 -- define a number1 variable which have add1 return value as its own value
 numberSum =
-  sum 2 3
+  (\x y -> x + y) 2 3
 -- main shows the index html page
 main =
     text (toString numberSum) -- toString converts an Int to String
