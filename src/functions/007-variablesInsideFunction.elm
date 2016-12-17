33cd8b8a03c1c9b162b17228c9f100dd2e9d9895
Adhy <adhywiranatap@gmail.com>
2016-12-17 21:38:43 +0700

    added let in function

--- src/functions/007-variablesInsideFunction.elm
+++ src/functions/007-variablesInsideFunction.elm
@@ -2,13 +2,13 @@ module VariablesInsideFunction exposing (..)
 
 import Html exposing (text)
 
-circleArea : Int -> Int
+circleArea : Int -> Float
 circleArea radius =
   let
     pi : Float
-    pi = 3.14
+    pi = 3.14 -- we "let" pi as a varible in the circleArea scope
   in
-    pi * radius^2
+    pi * (toFloat radius^2) -- float cannot be multiplied by Int, so we must toFloat the Int first!
 
 main =
   text (toString (circleArea 7))
