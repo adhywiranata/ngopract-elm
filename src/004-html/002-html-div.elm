f17ea38202a64dd9df0a1cbf99e37cc9f858c7c8
Adhy <adhywiranatap@gmail.com>
2017-03-29 13:08:26 +0700

    fixed 002 html div

--- src/004-html/002-html-div.elm
+++ src/004-html/002-html-div.elm
@@ -1,4 +1,5 @@
-import Html exposing (div, text, h1)
+import Html exposing (div, h1, text)
+import Html.Attributes exposing (class)
 
 main =
   div []
@@ -8,7 +9,7 @@ main =
       ]
     , div []
       [ text "text inside another nested div" ]
-    , div []
+    , div [ class "just-a-class-name" ]
       [ text "i'm kinda boring here.." ]
     , div []
       [ div [] [ text "nested nested again~" ] ]
