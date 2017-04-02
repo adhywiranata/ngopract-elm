cadd7b08a4b24da04a2da4bda43da9a488140895
Adhy <adhywiranatap@gmail.com>
2017-04-02 09:01:45 +0700

    updated pipelines module

--- src/003-functions/005-function-pipe-operator.elm
+++ src/003-functions/005-function-pipe-operator.elm
@@ -1,19 +1,46 @@
 module PipeOperator exposing (..)
 
-import Html exposing (text)
+import Html exposing (text, Html)
+
 
 add : Int -> Int -> Int
 add x y =
-  x + y
+    x + y
+
 
 subtract : Int -> Int -> Int
 subtract x y =
-  x - y
+    x - y
 
+
+multiply : Int -> Int -> Int
+multiply x y =
+    x * y
+
+
+divide : Int -> Int -> Int
+divide x y =
+    x // y
+
+
+abs : Int -> Int
+abs x =
+    x * -1
+
+
+finalNumber : Int
 finalNumber =
-  3
-    |> add 2 -- 2 + 3
-    |> subtract 1 -- subtract 1 (2 + 3)
--- main shows the index html page
+    3
+        |> add 2
+        -- 2 + 3
+        |> subtract 1
+        -- 1 - (2 + 3)
+        |> multiply 2
+        -- 2 * (1 - (2 + 3))
+        |> divide 16
+        -- 16 / ( 2 * (1 - (2 + 3)))
+        |> abs
+
+
 main =
-    text (toString finalNumber) -- 1 - ( 2 + 3 ) = -4
+    text (toString finalNumber)
