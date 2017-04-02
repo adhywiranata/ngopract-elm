083518afbf75b85dc3ae3242a7711d6820f7347a
Adhy <adhywiranatap@gmail.com>
2017-04-02 13:16:33 +0700

    updated decoding module

--- src/003-functions/012-1-decoding-single-integer-value.elm
+++ src/003-functions/012-1-decoding-single-integer-value.elm
@@ -4,14 +4,25 @@ import Html exposing (Html, h1, text)
 import Json.Decode as Decode
 
 
+-- here is a stringified json
+
+
 jsonStringified : String
 jsonStringified =
     "42"
 
 
+
+-- first, we need to create a decode function which return a Result
+
+
 decodedJson : String -> Result String Int
 decodedJson jsonStringified =
     Decode.decodeString Decode.int jsonStringified
+
+
+
+-- here, we "parse" the result to get the real value of the parsed number
 
 
 parseJsonNumber : Result String Int -> String
@@ -22,6 +33,10 @@ parseJsonNumber decoded =
 
         Result.Err errMessage ->
             "errMessage"
+
+
+
+-- display it in the main
 
 
 main : Html msg
