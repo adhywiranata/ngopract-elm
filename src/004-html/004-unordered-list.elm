16246d88716e8046862e6a64621c165fa09cab0b
Adhy <adhywiranatap@gmail.com>
2017-04-09 22:25:56 +0700

    updated ul module

--- src/004-html/004-unordered-list.elm
+++ src/004-html/004-unordered-list.elm
@@ -1,14 +1,17 @@
-import Html exposing (li, text, ul)
+module Main exposing (..)
+
+import Html exposing (Html, div, li, text, h1, ul)
 import Html.Attributes exposing (class, id)
 
+
+main : Html msg
 main =
-  ul [class "grocery-list"]
-    [ li [id "item-pamplemousee"] [text "Pamplemousse"]
-    , li [] [text "Ananas"]
-    , li [] [text "Jus d'orange"]
-    , li [] [text "Boeuf"]
-    , li [] [text "Soupe du jour"]
-    , li [] [text "Camembert"]
-    , li [] [text "Jacques Cousteau"]
-    , li [] [text "Baguette"]
-    ]
+    div [ id "container" ]
+        [ h1 [] [ text "Todo List Sederhana dengan Elm" ]
+        , ul [ class "todo-list" ]
+            [ li [] [ text "Belajar Elm" ]
+            , li [] [ text "Meeting dengan Budi" ]
+            , li [] [ text "Renovasi Rumah" ]
+            , li [] [ text "Service Mobil" ]
+            ]
+        ]
